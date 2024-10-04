8be3ea32621fd744371dac42b3b92bb7a4c05a8f
Chris Ackerman <chris@topher.land>
2024-10-03 23:48:09 -0700

    chore: add main package

---
+++ packages/main/README.md
@@ -0,0 +1,99 @@
+# @seahax/main
+
+Modify the NodeJS global context to better support CLI applications.
+
+Features:
+- Error stack trace source map support.
+- Uncaught error handling.
+- Console log level filtering.
+
+Table of Contents:
+- [Usage](#usage)
+- [Log Level](#log-level)
+- [Default Error Handler](#default-error-handler)
+- [Custom Error Handlers](#custom-error-handlers)
+- [Main Function](#main-function)
+
+
+## Usage
+
+Modify the global context.
+
+```ts
+import '@seahax/main';
+```
+
+## Log Level
+
+These levels are supported.
+
+- `error`
+- `warn`
+- `info`
+- `debug`
+
+Check the log level.
+
+```ts
+import { isLogLevel, LogLevel } from '@seahax/main';
+
+if (isLogLevel(LogLevel.warn)) {
+  // Do something only if "warn" level logging is enabled.
+}
+```
+
+Set the log level.
+
+```ts
+import { setLogLevel, LogLevel } from '@seahax/main';
+
+setLogLevel(LogLevel.warn);
+```
+
+The log level can also be set using the `LOG_LEVEL` environment variable.
+
+```sh
+process.env.LOG_LEVEL = 'warn';
+```
+
+## Default Error Handler
+
+The default error handler prints errors without stack traces, unless the log level is set to `debug`.
+
+Abort errors are not printed at all.
+
+## Custom Error Handlers
+
+Register custom error handlers for uncaught errors.
+
+```ts
+import { registerErrorHandler } from '@seahax/main';
+
+registerErrorHandler({ type: TypeError }, (error) => {
+  // Do something if the uncaught error is an instance of TypeError.
+});
+
+registerErrorHandler({ name: 'ErrorName'}, (error) => {
+  // Do something if the uncaught error "name" is "ErrorName".
+});
+
+registerErrorHandler({ code: 'ErrorCode'}, (error) => {
+  // Do something if the uncaught error "code" is "ErrorCode".
+});
+
+registerErrorHandler({ match: isSomeErrorType }, (error) => {
+  // Do something if the uncaught error matches a predicate.
+});
+```
+
+## Main Function
+
+Run a main function. Wrapping CLI logic in a function allows for async code without a top-level `await`. It also supports returning early without calling the `process.exit` function.
+
+```ts
+import { main } from '@seahax/main';
+
+main(async () => {
+  // Do something.
+});
+```
---
+++ packages/main/src/index.ts
@@ -0,0 +1,93 @@
+/* eslint-disable unicorn/no-process-exit */
+import sourceMapSupport from 'source-map-support';
+
+type LogArgs = [message?: any, ...optionalParams: any[]];
+type ErrorMatcher<TError> = ((error: unknown) => error is TError) | ((error: unknown) => boolean);
+
+interface ErrorConditions<TError> {
+  readonly name?: string | RegExp;
+  readonly code?: string | RegExp;
+  readonly type?: new (...args: any[]) => TError;
+  readonly match?: ErrorMatcher<TError>;
+}
+
+export enum LogLevel {
+  silent = 0,
+  error = 1,
+  warn = 2,
+  info = 3,
+  debug = 4,
+}
+
+const registeredErrorHandlers: { conditions: ErrorConditions<any>; handle: (error: any) => void }[] = [];
+
+export function main(action: () => void | Promise<void>): void {
+  void action();
+}
+
+export function isLogLevel(requiredLevel: LogLevel): boolean {
+  switch (process.env.LOG_LEVEL) {
+    case 'silent': {
+      return false;
+    }
+    case 'error': {
+      return requiredLevel >= LogLevel.error;
+    }
+    case 'warn': {
+      return requiredLevel >= LogLevel.warn;
+    }
+    case 'info': {
+      return requiredLevel >= LogLevel.info;
+    }
+    case 'debug': {
+      return requiredLevel >= LogLevel.debug;
+    }
+    default: {
+      return true;
+    }
+  }
+}
+
+export function registerErrorHandler<TError>(
+  conditions: ErrorConditions<TError> | ErrorMatcher<TError>,
+  handle: (error: TError) => void,
+): void {
+  conditions = typeof conditions === 'function' ? { match: conditions } : conditions;
+  registeredErrorHandlers.push({ conditions, handle: handle });
+}
+
+function defaultErrorHandler(error: any): void {
+  if (error?.name === 'AbortError') return;
+  console.error(isLogLevel(LogLevel.debug) ? error : String(error));
+}
+
+function uncaught(reason: any): void {
+  const handler = registeredErrorHandlers.find(({ conditions }) =>
+    (conditions.name != null && reason?.name === conditions.name)
+    || (conditions.code != null && reason?.code === conditions.code)
+    || (conditions.type && reason instanceof conditions.type)
+    || conditions.match?.(reason),
+  );
+
+  const handle = handler?.handle ?? defaultErrorHandler;
+
+  handle(reason);
+
+  process.exitCode ||= typeof reason?.exitCode === 'number' ? reason.exitCode : 1;
+  process.exit();
+}
+
+process.on('uncaughtException', uncaught);
+process.on('unhandledRejection', uncaught);
+
+const { error, warn, info, log, debug } = console;
+
+Object.assign(console, {
+  error: (...args: LogArgs) => void (isLogLevel(LogLevel.error) && warn.apply(console, args)),
+  warn: (...args: LogArgs) => void (isLogLevel(LogLevel.warn) && error.apply(console, args)),
+  info: (...args: LogArgs) => void (isLogLevel(LogLevel.info) && info.apply(console, args)),
+  log: (...args: LogArgs) => void (isLogLevel(LogLevel.info) && log.apply(console, args)),
+  debug: (...args: LogArgs) => void (isLogLevel(LogLevel.debug) && debug.apply(console, args)),
+});
+
+sourceMapSupport.install();
