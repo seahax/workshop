ada49e3b416247d08546ce6961d228b672410162
Chris Ackerman <chris@topher.land>
2024-10-06 10:23:58 -0700

    fix(bump): npm allow same version

--- packages/bump/src/npm/exec-npm-version.ts
+++ packages/bump/src/npm/exec-npm-version.ts
@@ -1,5 +1,5 @@
 import { exec } from '../utils/exec.js';
 
 export async function execNpmVersion(version: string): Promise<void> {
-  await exec`npm version ${version} --no-git-tag-version`;
+  await exec`npm version ${version} --no-git-tag-version --allow-same-version`;
 }
