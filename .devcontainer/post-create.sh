9bf5e651975dcf5b6d8e81afca0a68b920a916e8
Chris Ackerman <chris@topher.land>
2025-01-15 16:42:48 +0000

    add dotfiles import to container setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 set -e
+
+(
+  echo "Copying dotfiles..."
+  mkdir -p "$HOME/.dotfiles"
+  cd "$HOME/.dotfiles"
+  git clone "${GITHUB_SERVER_URL}/${GITHUB_USER}/devcontainer-dotfiles" .
+  rm -rf .git
+
+  if [ -f setup.sh ]; then
+    bash setup.sh
+    exit
+  fi
+
+  cp -rfv . ~
+) || echo "Failed to copy dotfiles."
+
 pnpm install
