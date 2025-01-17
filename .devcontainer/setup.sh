fe365e432d4f79302c37395020d955eaa4e925f2
Chris Ackerman <chris@topher.land>
2025-01-17 00:52:04 +0000

    install awscli from apt instead of as a devcontainer feature

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,10 @@ set -e
 unset LS_COLORS
 export NO_COLOR=1
 export TERM=xterm
+
+sudo apt-get update
+sudo apt-get install -y awscli
+
 export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
 npm i -g npm@latest
 npm i -g corepack
