30af0e3c3845303459290407b4048c09d93b6a90
Chris Ackerman <chris@topher.land>
2025-01-22 21:14:38 +0000

    add pnpm install to devcontainer setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -13,16 +13,19 @@ sudo apt-get update
 sudo apt-get install -y vim awscli
 
 # Install NVM and NodeJS LTS version.
-git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-cd "$NVM_DIR"
-git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
-. "$NVM_DIR/nvm.sh"
-nvm install --lts
+(
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
+  . "$NVM_DIR/nvm.sh"
+  nvm install --lts
+)
 
 # Install global NPM packages.
 [ -f "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 npm i -g npm@latest corepack@latest
 corepack enable
+pnpm install
 
 # Update the ZSH config.
 cat <<EOF >>"$HOME/.zshrc"
