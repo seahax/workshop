ae37106eb338b343bb6a3191aac97a394f4216a2
Chris Ackerman <chris@topher.land>
2025-02-02 07:57:14 +0000

    workflow stuff

---
+++ .github/workflows/setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+PATH="$HOME/bin:$PATH"
+PATH="$HOME/.asdf/bin:$PATH"
+
+PATHS=$(cat "$GITHUB_PATH")
+PATHS=<<EOF
+$HOME/bin
+$HOME/.asdf/bin
+$PATHS
+EOF
+echo "$PATHS" > "$GITHUB_PATH"
+cat "$GITHUB_PATH"
+
+wget https://github.com/asdf-vm/asdf/releases/download/v0.16.0/asdf-v0.16.0-linux-amd64.tar.gz -P "$HOME"
+
+mkdir -p "$HOME/bin"
+tar -xvf "$HOME/asdf-v0.16.0-linux-amd64.tar.gz" -C "$HOME/bin"
+
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add pnpm https://github.com/jonathanmorley/asdf-pnpm.git
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf install
+
+node -v
+pnpm -v
+golang version
