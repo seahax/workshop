b9f3d0c97b0dc7e9bb57093563dcdb2e824147f3
Chris Ackerman <chris@topher.land>
2025-02-02 08:17:05 +0000

    workflow stuff

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -7,7 +7,7 @@ echo "$ASDF_DATA_DIR/shims" >> "$GITHUB_PATH"
 echo "$ASDF_DIR" >> "$GITHUB_PATH"
 
 mkdir -p "$ASDF_DIR"
-wget https://github.com/asdf-vm/asdf/releases/download/v0.16.0/asdf-v0.16.0-linux-amd64.tar.gz -P "$ASDF_DIR"
+wget -q https://github.com/asdf-vm/asdf/releases/download/v0.16.0/asdf-v0.16.0-linux-amd64.tar.gz -P "$ASDF_DIR"
 tar -xvf "$ASDF_DIR/asdf-v0.16.0-linux-amd64.tar.gz" -C "$ASDF_DIR"
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
