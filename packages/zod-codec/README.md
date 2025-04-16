e7392cdae285c54102cbe7033594479c9257e462
Chris Ackerman <chris@topher.land>
2025-04-16 04:53:16 +0000

    feat: refactor zod-codec

--- packages/zod-codec/README.md
+++ packages/zod-codec/README.md
@@ -1,6 +1,6 @@
 # @seahax/zod-codec
 
-Bi-directional transformation between two Zod schemas (ie. codec, encode and decode).
+Bi-directional transformation between two Zod schemas (ie. encode and decode, codec).
 
 ## Example
 
@@ -9,17 +9,29 @@ Declare a codec which translates bi-directionally between a `Date` and a `string
 ```ts
 import { zodCodec } from '@seahax/zod-codec';
 
-const codec = zodCodec({
-  encoded: z.instanceof(Date),
-  decoded: z.string().datetime(),
-  encode: (decoded) => Date.parse(decoded),
-  decode: (encoded) => encoded.toISOString(),
+const [DATE, DATE_STRING] = zodCodec(
+  {
+    input: z.instanceof(Date),
+    transform: (input) => {
+      // The input is typed as Date.
+      // The return type must be the other input type (ISO datetime string).
+      return input.toISOString();
+    },
+  },
+  {
+    input: z.string().datetime(),
+    transform: (input) => {
+      // The input is typed as an ISO datetime string.
+      // The return type must be the other input type (Date).
+      return Date.parse(input)
+    },
+  },
 });
 ```
 
 Use the codec to transform (encode/decode) dates to strings and vice versa.
 
 ```ts
-const date: Date = codec.encode('2023-01-01T00:00:00Z');
-const isoString: string = codec.decode(date);
+const date: Date = DATE.parse('2023-01-01T00:00:00Z');
+const dateString: string = DATE_STRING.parse(date);
 ```
--- packages/zod-codec/src/index.ts
+++ packages/zod-codec/src/index.ts
@@ -1,64 +1,30 @@
 import type { z } from 'zod';
 
-const ZOD_CODEC_SCHEMA = Symbol('ZodCodecSchema');
-
-export type ZodCodecSchema<T> = z.ZodType<T> & {
-  [ZOD_CODEC_SCHEMA]: unknown;
+export interface ZodCodecType<TInput, TOutput> extends z.ZodEffects<z.ZodType<TInput>, TOutput> {
+  readonly input: z.ZodType<TInput>;
 };
 
-export interface ZodCodec<TEncoded, TDecoded> {
-  /**
-   * Zod schema which parses an "encoded" value of the paired `encoded` schema
-   * type, and transforms (ie. decodes) it into this type.
-   */
-  readonly decoded: ZodCodecSchema<TDecoded>;
-  /**
-   * Zod schema which parses a "decoded" value of the paired `decoded` schema
-   * type, and transforms (ie. encodes) it into this type.
-   */
-  readonly encoded: ZodCodecSchema<TEncoded>;
+export interface ZodCodecEntry<TInput, TOutput> {
+  readonly input: z.ZodType<TInput>;
+  readonly transform: (value: TInput) => TOutput;
 }
 
-export interface ZodCodecConfig<
-  TEncoded extends z.ZodTypeAny,
-  TDecoded extends z.ZodTypeAny,
-> {
-  /**
-   * Zod schema for the encoded value.
-   */
-  readonly encoded: TEncoded;
-  /**
-   * Zod schema for the decoded value.
-   */
-  readonly decoded: TDecoded;
-  /**
-   * Transform an unencoded (decoded) value to an encoded value.
-   */
-  readonly encode: (value: z.infer<TDecoded>) => z.infer<TEncoded>;
-  /**
-   * Transform an encoded value to an unencoded (decoded) value.
-   */
-  readonly decode: (value: z.infer<TEncoded>) => z.infer<TDecoded>;
-}
+export type ZodCodecPair<T0, T1> = readonly [
+  ZodCodecType<T0, T1>,
+  ZodCodecType<T1, T0>,
+];
 
-/**
- * Create a pair of Zod schemas with reciprocal transformations.
- *
- * The two schemas are reciprocal because:
- * - The returned `encoded` schema parses the `decoded` schema type and
- *   transforms it to the `encoded` schema type.
- * - The returned `decoded` schema parses the `encoded` schema type and
- *   transforms it to the `decoded` schema type.
- */
-export default function zodCodec<TEncoded extends z.ZodTypeAny, TDecoded extends z.ZodTypeAny>(
-  options: ZodCodecConfig<TEncoded, TDecoded>,
-): ZodCodec<z.infer<TEncoded>, z.infer<TDecoded>> {
-  return {
-    decoded: Object.assign(options.encoded.transform((value) => options.decoded.parse(options.decode(value))), {
-      [ZOD_CODEC_SCHEMA]: true,
-    }),
-    encoded: Object.assign(options.decoded.transform((value) => options.encoded.parse(options.encode(value))), {
-      [ZOD_CODEC_SCHEMA]: true,
-    }),
-  };
+export function zodCodec<T0, T1>(
+  ...[entry0, entry1]: [ZodCodecEntry<T0, NoInfer<T1>>, ZodCodecEntry<T1, NoInfer<T0>>]
+): ZodCodecPair<T0, T1> {
+  return [
+    Object.assign(
+      entry0.input.transform((input) => entry1.input.parse(entry0.transform(input))),
+      { input: entry0.input },
+    ),
+    Object.assign(
+      entry1.input.transform((input) => entry0.input.parse(entry1.transform(input))),
+      { input: entry1.input },
+    ),
+  ];
 }
