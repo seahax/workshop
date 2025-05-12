8df69131575667588e268276453b0ab5b531d443
Chris Ackerman <chris@topher.land>
2025-05-12 19:47:45 +0000

    feat: main does not return a promise

--- packages/main/src/main.ts
+++ packages/main/src/main.ts
@@ -1,10 +1,12 @@
 /* eslint-disable unicorn/no-process-exit */
+import { nextTick } from 'node:process';
+
 import sourceMapSupport from 'source-map-support';
 
 import { events, initEvents } from './events.ts';
 import { initLog } from './log.ts';
 
-async function main(action: () => void | Promise<void>): Promise<never> {
+function main(action: () => void | Promise<void>): void {
   if ('called' in main) throw new Error('The main function can only be called once.');
 
   Object.assign(main, { called: true });
@@ -12,16 +14,17 @@ async function main(action: () => void | Promise<void>): Promise<never> {
   initLog();
   initEvents();
 
-  try {
-    await action();
-  }
-  catch (error: any) {
-    if (error?.name !== 'AbortError') {
-      events.emit('uncaughtError', error);
-    }
-  }
-
-  return process.exit();
+  // Using nextTick prevents "used before defined" issues.
+  void new Promise<void>((resolve) => nextTick(() => resolve()))
+    .then(action).catch((error: unknown) => {
+      if ((error as any)?.name !== 'AbortError') {
+        events.emit('uncaughtError', error);
+      }
+    })
+    .finally(() => {
+      // When the action resolves, the process must exit.
+      return process.exit();
+    });
 }
 
 export default Object.assign(main, {
