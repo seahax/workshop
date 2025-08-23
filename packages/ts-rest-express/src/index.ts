b805bab53cf570a9aca418e35cadf70613bdf793
Chris Ackerman <chris@topher.land>
2025-08-23 05:53:48 +0000

    remove ts-rest packages

--- packages/ts-rest-express/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './config.ts';
-export * from './errors.ts';
-export * from './handler.ts';
-export type * from './request.ts';
-export type * from './response.ts';
-export * from './routes.ts';
--- packages/ts-rest-express/src/types.ts
+++
@@ -1,9 +0,0 @@
-type Simplify<T> = T extends object ? { [K in keyof T]: T[K] } : T;
-
-export type WithDefault<A, B> = unknown extends A ? B : A;
-
-export type SmartPartial<T extends object> = Simplify<{
-  [K in keyof T as undefined extends T[K] ? K : never]+?: T[K]
-} & {
-  [K in keyof T as undefined extends T[K] ? never : K]-?: T[K]
-}>;
