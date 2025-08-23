7306bdaf022ab836b1de269a84a8854215515e7f
Chris Ackerman <chris@topher.land>
2025-08-23 17:33:35 +0000

    remove express packages

--- packages/express-info/CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-
-## 0.1.1 - 2025-04-23
-
-- __chore:__ refactor all the things (3211a53)
--- packages/express-info/src/index.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestHandler } from 'express';
-
-export default function createInfoHandler(
-  data: Record<string, string | number | bigint | boolean | null | undefined>,
-): RequestHandler {
-  return (req, res) => void res.json(data);
-}
