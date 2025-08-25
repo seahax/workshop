caf3b8f04f49c31010145291023cbc1b1fa25d4b
Chris Ackerman <chris@topher.land>
2025-08-25 20:15:38 +0000

    feat(background): add onBegin and onSuccess handlers

--- packages/background/src/index.ts
+++ packages/background/src/index.ts
@@ -1,46 +1,90 @@
 declare const console: { error(...data: unknown[]): void } | undefined;
 
-type Background<TArgs extends unknown[]> = (task: () => Promise<void>, ...args: TArgs) => BackgroundTask;
+type BackgroundFunction<TArgs extends unknown[]> = (task: () => Promise<void>, ...args: TArgs) => BackgroundTask;
+
+interface BackgroundProperties<TArgs extends unknown[]> {
+  /**
+   * Add a callback to be invoked when the task begins.
+   */
+  onBegin(callback: BackgroundCallback<TArgs>): this;
+
+  /**
+   * Add a callback to be invoked when the task succeeds.
+   */
+  onSuccess(callback: BackgroundCallback<TArgs>): this;
+}
+
+export type Background<TArgs extends unknown[]> = BackgroundFunction<TArgs> & BackgroundProperties<TArgs>;
 
 export interface BackgroundTask {
   /**
    * Get the current status of the task.
    */
-  readonly status: 'pending' | 'resolved' | 'rejected';
+  readonly status: BackgroundTaskStatus;
+
   /**
    * Returns a promise that resolves when the task is finished.
    */
   finished(): Promise<'resolved' | 'rejected'>;
 }
 
-export function createBackground<TArgs extends unknown[]>(
-  errorHandler: (error: unknown, ...args: TArgs) => void | Promise<void>,
+export type BackgroundTaskStatus = 'pending' | 'resolved' | 'rejected';
+export type BackgroundCallback<TArgs extends unknown[]> = (...args: TArgs) => void | Promise<void>;
+export type BackgroundErrorHandler<TArgs extends unknown[]> = (error: unknown, ...args: TArgs) => void | Promise<void>;
+
+export function createBackground<TArgs extends unknown[] = []>(
+  onError: BackgroundErrorHandler<TArgs>,
 ): Background<TArgs> {
-  return (task, ...args) => {
-    let status: BackgroundTask['status'] = 'pending';
+  const onBeginCallbacks: BackgroundCallback<TArgs>[] = [];
+  const onSuccessCallbacks: BackgroundCallback<TArgs>[] = [];
+  const self: Background<TArgs> = Object.assign<BackgroundFunction<TArgs>, BackgroundProperties<TArgs>>(
+    (task, ...args) => {
+      let status: BackgroundTaskStatus = 'pending';
 
-    const promise = task()
-      .then(() => {
-        return status = 'resolved' as const;
-      })
-      .catch(async (error: unknown) => {
-        try {
-          await errorHandler(error, ...args);
-        }
-        catch (error) {
-          console?.error('Error handling background error:', error);
-        }
+      const promise = Promise.resolve()
+        .then(async () => {
+          for (const onBegin of onBeginCallbacks) {
+            await onBegin(...args);
+          }
 
-        return status = 'rejected' as const;
-      });
+          await task();
 
-    return {
-      get status() {
-        return status;
+          for (const onSuccess of onSuccessCallbacks) {
+            await onSuccess(...args);
+          }
+
+          return status = 'resolved' as const;
+        })
+        .catch(async (error: unknown) => {
+          try {
+            await onError(error, ...args);
+          }
+          catch (error) {
+            console?.error('Error handling background error:', error);
+          }
+
+          return status = 'rejected' as const;
+        });
+
+      return {
+        get status() {
+          return status;
+        },
+        async finished() {
+          return await promise;
+        },
+      };
+    }, {
+      onBegin(callback) {
+        onBeginCallbacks.push(callback);
+        return self;
       },
-      async finished() {
-        return await promise;
+      onSuccess(callback) {
+        onSuccessCallbacks.push(callback);
+        return self;
       },
-    };
-  };
+    },
+  );
+
+  return self;
 }
