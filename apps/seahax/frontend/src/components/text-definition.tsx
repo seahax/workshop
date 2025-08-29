a1a966142b6c7dccc976bd7ebc5e624e9c60414d
Chris Ackerman <chris@topher.land>
2025-08-29 02:59:25 +0000

    open definition tooltip on touch

--- apps/seahax/frontend/src/components/text-definition.tsx
+++ apps/seahax/frontend/src/components/text-definition.tsx
@@ -1,5 +1,5 @@
-import { Box, Tooltip } from '@mui/material';
-import { type JSX, type TouchEventHandler, useCallback } from 'react';
+import { Box, ClickAwayListener, Tooltip } from '@mui/material';
+import { type JSX, useCallback } from 'react';
 
 import useBoolean from '../hooks/use-boolean.ts';
 
@@ -10,23 +10,27 @@ interface Props {
 
 export default function TextDefinition({ term, definition }: Props): JSX.Element {
   const { value: isFocused, setTrue: focus, setFalse: blur } = useBoolean();
-  const onTouchEnd = useCallback<TouchEventHandler<HTMLSpanElement>>((event) => {
-    event.currentTarget.focus();
-  }, []);
+  const onTouchStart = useCallback(() => focus(), [focus]);
 
   return (
-    <Tooltip title={definition} open={isFocused || undefined} disableHoverListener={isFocused} disableTouchListener>
-      <Box
-        component="span"
-        tabIndex={0}
-        borderBottom={(theme) => `3px dotted ${theme.palette.primary.main}`}
-        marginBottom="-4px"
-        onFocus={focus}
-        onBlur={blur}
-        onTouchEnd={onTouchEnd}
-      >
-        {term}
-      </Box>
+    <Tooltip
+      title={definition}
+      open={isFocused || undefined}
+      disableHoverListener={isFocused}
+      disableTouchListener={isFocused}
+      disableFocusListener={isFocused}
+    >
+      <ClickAwayListener onClickAway={blur}>
+        <Box
+          component="span"
+          tabIndex={0}
+          borderBottom={(theme) => `3px dotted ${theme.palette.primary.main}`}
+          marginBottom="-4px"
+          onTouchStart={onTouchStart}
+        >
+          {term}
+        </Box>
+      </ClickAwayListener>
     </Tooltip>
   );
 }
