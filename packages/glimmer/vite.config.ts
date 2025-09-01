2984550dba5c8d070a445b7a488035d8b5097568
Chris Ackerman <chris@topher.land>
2025-09-01 19:41:38 +0000

    fix(app-seahax): dev environment not configured for dev (frontend)

---
+++ packages/glimmer/vite.config.ts
@@ -0,0 +1,13 @@
+import lib from '@seahax/vite-plugin-lib';
+import { defineConfig } from 'vite';
+
+process.chdir(import.meta.dirname);
+
+export default defineConfig({
+  plugins: [
+    lib({ runtime: 'browser', bundle: false }),
+  ],
+  server: {
+    host: '127.0.0.1',
+  },
+});
