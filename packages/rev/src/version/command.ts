2f478bfd10851cf365873e7e7c375968edfd5659
Chris Ackerman <chris@topher.land>
2025-09-14 18:58:47 +0000

    feat(rev): write a JSON release manifest file

--- packages/rev/src/version/command.ts
+++ packages/rev/src/version/command.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-import { alias, createHelp, cue, flag, parseOptions } from '@seahax/args';
+import fs from 'node:fs/promises';
+
+import { alias, createHelp, cue, flag, option, parseOptions } from '@seahax/args';
 import { getPackages } from '@seahax/monorepo';
 
 import { printResult } from './print-result.ts';
@@ -16,16 +18,20 @@ Read the docs for more information on commit message formating:
 https://github.com/seahax/workshop/blob/main/packages/rev/README.md#version
 
 {bold Options:}
-  {blue --force}         Bump the version even if there are no new commits.
-  {blue --allow-dirty}   Allow the Git working directory to be dirty.
-  {blue --dry-run}       Do not write any files.
-  {blue --help, -h}      Show this help message.
+  {blue --force}             Bump the version even if there are no new commits.
+  {blue --allow-dirty}       Allow the Git working directory to be dirty.
+  {blue --release-file}      Release manifest file (default: .release.json).
+  {blue --no-release-file}   Do not write a release manifest file.
+  {blue --dry-run}           Do not write any files.
+  {blue --help, -h}          Show this help message.
 `;
 
 export async function versionCommand(args: string[]): Promise<void> {
   const options = await parseOptions(args, {
     '--force': flag(),
     '--allow-dirty': flag(),
+    '--release-file': option(),
+    '--no-release-file': flag(),
     '--dry-run': flag(),
     '--help': cue(),
     '-h': alias('--help'),
@@ -44,6 +50,8 @@ export async function versionCommand(args: string[]): Promise<void> {
   const {
     '--force': force,
     '--allow-dirty': allowDirty,
+    '--release-file': releaseFile = '.release.json',
+    '--no-release-file': noReleaseFile,
     '--dry-run': dryRun,
   } = options.value;
   const packages = await getPackages(process.cwd());
@@ -57,7 +65,19 @@ export async function versionCommand(args: string[]): Promise<void> {
 
   if (dryRun) return;
 
+  const released: string[] = [];
+
   await Promise.all(Array.from(results.values()).map(async (result) => {
-    if ('commit' in result) await result.commit();
+    if (!('commit' in result)) return;
+
+    await result.commit();
+    released.push(`${result.name}@${result.versions.to}`);
   }));
+
+  if (!noReleaseFile) {
+    await fs.writeFile(releaseFile, JSON.stringify({
+      date: new Date().toISOString(),
+      released,
+    }, null, 2), 'utf8');
+  }
 }
