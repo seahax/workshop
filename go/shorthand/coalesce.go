9346c63cf58dd0d4024643005a2f2b508a72509d
Chris Ackerman <chris@topher.land>
2025-10-26 21:14:31 +0000

    add CoalesceFunc to go/shorthand

--- go/shorthand/coalesce.go
+++ go/shorthand/coalesce.go
@@ -14,3 +14,17 @@ func Coalesce[T any](values ...T) T {
 	var zero T
 	return zero
 }
+
+func CoalesceFunc[T any](funcs ...func() T) T {
+	for _, fn := range funcs {
+		value := fn()
+		reflectValue := reflect.ValueOf(value)
+
+		if reflectValue.IsValid() && !reflectValue.IsZero() {
+			return value
+		}
+	}
+
+	var zero T
+	return zero
+}
