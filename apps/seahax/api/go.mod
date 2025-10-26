d778f8a0bf1b4bb7037052e8c8f6798da450ca4b
Chris Ackerman <chris@topher.land>
2025-10-26 07:50:11 +0000

    update apps/seahax deps and improve config handling

--- apps/seahax/api/go.mod
+++ apps/seahax/api/go.mod
@@ -3,8 +3,9 @@ module seahax/api
 go 1.25.3
 
 require (
-	github.com/seahax/workshop/go/api v0.0.4
-	github.com/seahax/workshop/go/env v0.0.1
+	github.com/go-playground/validator/v10 v10.28.0
+	github.com/seahax/workshop/go/api v0.0.5
+	github.com/seahax/workshop/go/env v0.0.2
 )
 
 require (
@@ -12,10 +13,9 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.10 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.28.0 // indirect
 	github.com/klauspost/compress v1.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/seahax/workshop/go/defaults v0.0.1 // indirect
+	github.com/seahax/workshop/go/defaults v0.0.2 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
--- apps/seahax/api/services/config/config.go
+++ apps/seahax/api/services/config/config.go
@@ -1,58 +1,71 @@
 package config
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"os"
+	"sync"
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/seahax/workshop/go/env"
 )
 
 type Config struct {
-	// The timestamp when the application started.
-	StartTimestamp int64 `env:"ignore"`
-
 	// The timestamp when the application was built.
-	BuildTimestamp int64
+	BuildTimestamp int64 `env:"APP_BUILD_TIMESTAMP"`
+
+	// The timestamp when the application started.
+	StartTimestamp int64
 
 	// The commit hash of the application build.
-	Commit string
+	Commit string `env:"APP_COMMIT"`
 
 	// The application environment (eg. development, production).
-	Environment string
+	Environment string `env:"APP_ENVIRONMENT" validate:"required,oneof=development production"`
 
 	// The hostname or IP address the application binds to.
-	Hostname string `validate:"isdefault|ipv4|ipv6|hostname"`
+	Hostname string `env:"APP_HOSTNAME" validate:"isdefault|ipv4|ipv6|hostname"`
 
 	// The port number the application binds to.
-	Port uint `validate:"required,port"`
+	Port uint `env:"APP_PORT" validate:"required,port"`
 
 	// The origin URL of the application.
-	Origin string `validate:"required,url"`
+	Origin string `env:"APP_ORIGIN" validate:"required,url"`
 
 	// The path to the static files served by the application.
-	StaticPath string `validate:"required,dir"`
+	StaticPath string `env:"APP_STATIC_PATH" validate:"required,dir"`
 }
 
 func (c Config) Address() string {
 	return fmt.Sprintf("%s:%d", c.Hostname, c.Port)
 }
 
-var singleton *Config
+var Get = sync.OnceValue(func() *Config {
+	now := time.Now().UnixMilli()
+	cfg := &Config{
+		BuildTimestamp: now,
+		StartTimestamp: now,
+		Environment:    "development",
+	}
 
-func Get() *Config {
-	if singleton == nil {
-		singleton = &Config{
-			StartTimestamp: time.Now().UnixMilli(),
-			Environment:    "development",
-		}
+	if err := env.Bind(cfg); err != nil {
+		log.Fatalln(err)
+	}
 
-		if err := env.BindPrefixedEnv("APP_", singleton); err != nil {
-			log.Fatalln(err)
+	if err := validator.New().Struct(cfg); err != nil {
+		var errs validator.ValidationErrors
+		if errors.As(err, &errs) {
+			for _, err := range errs {
+				env := env.GetTag(cfg, err.StructField())
+				log.Printf(`invalid config '%s' (%s)`, env, err.ActualTag())
+			}
+			os.Exit(1)
 		}
 	}
 
-	return singleton
-}
+	return cfg
+})
 
 // TODO: Add MongoDB client.
