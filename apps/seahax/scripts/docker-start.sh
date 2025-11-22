e3b29691a2006035ac090246e5ea2642244eeb16
Chris Ackerman <chris@topher.land>
2025-11-22 10:54:51 +0000

    fix apps/seahax/api go build task not including init file

--- apps/seahax/scripts/docker-start.sh
+++ apps/seahax/scripts/docker-start.sh
@@ -3,7 +3,8 @@ set -e
 [ -z "$npm_package_name" ] && echo "npm_package_name is not set" && exit 1
 docker run -it --rm \
   -e APP_ORIGIN=http://127.0.0.1:8080 \
-  -e APP_DATABASE_URL="${APP_DATABASE_URL}" \
+  -e APP_MONGODB_URL="${APP_MONGODB_URL}" \
+  -e APP_ENVIRONMENT=development \
   -p 8080:8080 \
   "$npm_package_name" \
   "$@"
