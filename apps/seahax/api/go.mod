24cad96756df4abe81a04b73a616b0e3a30408ad
Chris Ackerman <chris@topher.land>
2025-11-25 07:00:19 +0000

    update apps/seahax/api deps

--- apps/seahax/api/go.mod
+++ apps/seahax/api/go.mod
@@ -3,10 +3,10 @@ module seahax/api
 go 1.25.3
 
 require (
-	github.com/getsentry/sentry-go v0.38.0
+	github.com/getsentry/sentry-go v0.39.0
 	go.mongodb.org/mongo-driver/v2 v2.4.0
-	seahax.com/go/env v0.0.8
-	seahax.com/go/shorthand v0.0.8
+	seahax.com/go/env v0.0.9
+	seahax.com/go/shorthand v0.0.11
 	seahax.com/go/xhttp v0.0.3
 )
 
@@ -20,7 +20,7 @@ require (
 	github.com/klauspost/compress v1.18.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
-	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/scram v1.2.0 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
--- apps/seahax/api/internal/config/config.go
+++ apps/seahax/api/internal/config/config.go
@@ -8,7 +8,7 @@ import (
 	"seahax.com/go/shorthand"
 )
 
-func Get[T any](name string, tags ...string) T {
+func GetEnv[T any](name string, tags ...string) T {
 	return shorthand.CriticalValue(env.Get[T](name,
 		env.OptionPrefix("APP_"),
 		env.OptionValidate(tags...),
@@ -16,13 +16,13 @@ func Get[T any](name string, tags ...string) T {
 }
 
 var StartTimestamp = time.Now().UnixMilli()
-var BuildTimestamp = Get[int64]("BUILD_TIMESTAMP")
-var Commit = Get[string]("COMMIT")
-var Environment = Get[string]("ENVIRONMENT", "oneof=development production")
-var Address = Get[string]("ADDRESS")
-var Origin = Get[string]("ORIGIN", "url")
-var StaticPath = Get[string]("STATIC_PATH", "dir")
-var LogLevel = Get[slog.Level]("LOG_LEVEL")
-var LogFormat = shorthand.Coalesce(Get[string]("LOG_FORMAT", "omitempty", "oneof=text json"), "text")
-var SentryDSN = Get[string]("SENTRY_DSN", "omitempty", "url")
-var MongoURI = Get[string]("MONGODB_URL", "url")
+var BuildTimestamp = GetEnv[int64]("BUILD_TIMESTAMP")
+var Commit = GetEnv[string]("COMMIT")
+var Environment = GetEnv[string]("ENVIRONMENT", "oneof=development production")
+var Address = GetEnv[string]("ADDRESS")
+var Origin = GetEnv[string]("ORIGIN", "url")
+var StaticPath = GetEnv[string]("STATIC_PATH", "dir")
+var LogLevel = GetEnv[slog.Level]("LOG_LEVEL")
+var LogFormat = shorthand.Coalesce(GetEnv[string]("LOG_FORMAT", "omitempty", "oneof=text json"), "text")
+var SentryDSN = GetEnv[string]("SENTRY_DSN", "omitempty", "url")
+var MongoURI = GetEnv[string]("MONGODB_URL", "url")
