9c506fc343a7797db62e8cfcde780751dd9de67a
Chris Ackerman <chris@topher.land>
2025-12-30 18:09:21 +0000

    add DO Spaces (S3) bucket to apps/seahax

--- apps/seahax/terraform/app.tf
+++ apps/seahax/terraform/app.tf
@@ -2,79 +2,92 @@ resource "digitalocean_app" "self" {
   project_id = data.digitalocean_project.self.id
 
   spec {
-    name = "seahax"
+    name   = "seahax"
     region = "sfo"
 
     service {
-      name = "app"
-      http_port = 8080
-      instance_count = 1
+      name               = "app"
+      http_port          = 8080
+      instance_count     = 1
       instance_size_slug = "apps-s-1vcpu-0.5gb"
 
       image {
-        registry_type = "GHCR"
+        registry_type        = "GHCR"
         registry_credentials = "seahax:${var.ghcr_token}"
-        registry = "seahax"
-        repository = "app"
-        tag = "latest"
+        registry             = "seahax"
+        repository           = "app"
+        tag                  = "latest"
       }
 
       env {
-        key = "APP_ENVIRONMENT"
+        key   = "APP_ENVIRONMENT"
         scope = "RUN_TIME"
         value = local.environment
       }
 
       env {
-        key = "APP_MONGODB_URL"
+        key   = "APP_MONGODB_URL"
         scope = "RUN_TIME"
         value = "$${db.DATABASE_URL}"
       }
 
       env {
-        key = "APP_CACHE_URL"
+        key   = "APP_CACHE_URL"
         scope = "RUN_TIME"
         value = "$${cache.REDIS_URL}"
       }
 
       env {
-        key = "APP_SENTRY_DSN"
+        key   = "APP_SPACES_ACCESS_KEY_ID"
+        scope = "RUN_TIME"
+        value = digitalocean_spaces_key.bucket-read.access_key
+      }
+
+      env {
+        key   = "APP_SPACES_SECRET_ACCESS_KEY"
+        scope = "RUN_TIME"
+        value = digitalocean_spaces_key.bucket-read.secret_key
+        type  = "SECRET"
+      }
+
+      env {
+        key   = "APP_SENTRY_DSN"
         scope = "RUN_TIME"
         value = local.sentry_dsn
       }
 
       env {
-        key = "APP_SMTP_SERVER"
+        key   = "APP_SMTP_SERVER"
         scope = "RUN_TIME"
         value = local.smtp_server
       }
 
       env {
-        key = "APP_SMTP_PORT"
+        key   = "APP_SMTP_PORT"
         scope = "RUN_TIME"
         value = tostring(local.smtp_port)
       }
 
       env {
-        key = "APP_SMTP_USERNAME"
+        key   = "APP_SMTP_USERNAME"
         scope = "RUN_TIME"
         value = local.smtp_username
       }
 
       env {
-        key = "APP_SMTP_TOKEN"
+        key   = "APP_SMTP_TOKEN"
         scope = "RUN_TIME"
         value = var.smtp_token
-        type = "SECRET"
+        type  = "SECRET"
       }
 
       health_check {
-        http_path = "/_health"
+        http_path             = "/_health"
         initial_delay_seconds = 15
-        period_seconds = 30
-        timeout_seconds = 3
-        success_threshold = 1
-        failure_threshold = 3
+        period_seconds        = 30
+        timeout_seconds       = 3
+        success_threshold     = 1
+        failure_threshold     = 3
       }
     }
 
@@ -102,19 +115,19 @@ resource "digitalocean_app" "self" {
     }
 
     database {
-      name = "db"
+      name         = "db"
       cluster_name = digitalocean_database_cluster.mongodb.name
-      engine = upper(digitalocean_database_cluster.mongodb.engine)
-      version = digitalocean_database_cluster.mongodb.version
-      production = true
+      engine       = upper(digitalocean_database_cluster.mongodb.engine)
+      version      = digitalocean_database_cluster.mongodb.version
+      production   = true
     }
 
     database {
-      name = "cache"
+      name         = "cache"
       cluster_name = digitalocean_database_cluster.valkey.name
-      engine = upper(digitalocean_database_cluster.valkey.engine)
-      version = digitalocean_database_cluster.valkey.version
-      production = true
+      engine       = upper(digitalocean_database_cluster.valkey.engine)
+      version      = digitalocean_database_cluster.valkey.version
+      production   = true
     }
 
     alert { rule = "DEPLOYMENT_FAILED" }
--- apps/seahax/terraform/provider.tf
+++ apps/seahax/terraform/provider.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
@@ -12,17 +12,17 @@ terraform {
     endpoints = {
       s3 = "https://sfo3.digitaloceanspaces.com"
     }
-    
+
     bucket = "seahax-tfstate"
-    key = "seahax"
+    key    = "seahax"
 
     # Deactivate a few AWS-specific checks
     skip_credentials_validation = true
-    skip_requesting_account_id = true
-    skip_metadata_api_check = true
-    skip_region_validation = true
-    skip_s3_checksum = true
-    region = "us-east-1"
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+    region                      = "us-east-1"
 
     # Enable state locking with a lockfile
     use_lockfile = true
@@ -30,5 +30,4 @@ terraform {
 }
 
 provider "digitalocean" {
-  # token = var.token
 }
