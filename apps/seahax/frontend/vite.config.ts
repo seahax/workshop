f36fef160246be2489b7678dd87f6381bb9f5d45
Chris Ackerman <chris@topher.land>
2025-12-31 02:45:31 +0000

    add apps/seahax/frontend handling for entry script load failures

--- apps/seahax/frontend/vite.config.ts
+++ apps/seahax/frontend/vite.config.ts
@@ -2,28 +2,18 @@ import mdx from '@mdx-js/rollup';
 import prefetch from '@seahax/vite-plugin-prefetch';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
 
 import projects from './config/projects.ts';
 
 process.chdir(import.meta.dirname);
 
-const sentry = process.env.SENTRY_AUTH_TOKEN
-  ? sentryVitePlugin({
-      org: 'seahax',
-      project: 'seahax-frontend',
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    })
-  : undefined;
-
-console.log(`Sentry Vite Plugin: ${sentry ? 'enabled' : 'disabled'}`);
-
-export default defineConfig((env) => {
+export default defineConfig(async (env) => {
   process.env.NODE_ENV ??= process.env.VITE_MODE ?? env.mode;
 
   return {
     mode: process.env.NODE_ENV,
-    plugins: [react(), mdx(), sentry, prefetch()],
+    plugins: [stripHtmlComments, react(), mdx(), sentry, prefetch()],
     build: {
       sourcemap: true,
       modulePreload: false,
@@ -37,3 +27,22 @@ export default defineConfig((env) => {
     },
   };
 });
+
+const stripHtmlComments: Plugin = {
+  name: 'strip-html-comments',
+  transformIndexHtml: {
+    handler(html) {
+      return html.replaceAll(/\s*<!--.*?-->/gsu, '');
+    },
+  },
+};
+
+const sentry = process.env.SENTRY_AUTH_TOKEN
+  ? sentryVitePlugin({
+      org: 'seahax',
+      project: 'seahax-frontend',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    })
+  : undefined;
+
+console.log(`Sentry Vite Plugin: ${sentry ? 'enabled' : 'disabled'}`);
