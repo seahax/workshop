ff61e771c63323e20b42a669fddad838bc8fa7c8
Chris Ackerman <chris@topher.land>
2025-12-31 02:46:09 +0000

    add local mongodb support to apps/seahax

--- apps/seahax/scripts/docker-start.sh
+++ apps/seahax/scripts/docker-start.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 set -e
 [ -z "$npm_package_name" ] && echo "npm_package_name is not set" && exit 1
+docker network create seahax || true
 docker run -it --rm \
+  --name seahax-app \
+  --network seahax \
+  -p 8080:8080 \
   -e APP_ENVIRONMENT=development \
-  -e APP_MONGODB_URL="${APP_MONGODB_URL}" \
+  -e APP_MONGODB_URL="mongodb://root:${MONGO_ROOT_PASSWORD}@seahax-mongodb:27017/admin?authSource=admin" \
   -e APP_SMTP_SERVER="${APP_SMTP_SERVER}" \
   -e APP_SMTP_PORT="${APP_SMTP_PORT}" \
   -e APP_SMTP_USERNAME="${APP_SMTP_USERNAME}" \
   -e APP_SMTP_TOKEN="${APP_SMTP_TOKEN}" \
-  -p 8080:8080 \
+  -e APP_LOG_LEVEL="${APP_LOG_LEVEL}" \
   "$npm_package_name" \
   "$@"
